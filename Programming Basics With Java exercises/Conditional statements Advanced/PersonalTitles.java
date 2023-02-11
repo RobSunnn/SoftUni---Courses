713d2f257508097a0d16378becefb7296ae638fb
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-02-11 12:21:27 +0200

    Added more exercises from programming basics course in software university SoftUni

---
+++ Programming Basics With Java exercises/Conditional statements Advanced/PersonalTitles.java
@@ -0,0 +1,32 @@
+package ConditionalStatementAdvancedLab;
+
+import java.util.Scanner;
+
+public class PersonalTitles {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double age = Double.parseDouble(scan.nextLine());
+        String gender = scan.nextLine();
+
+        if ("m".equals(gender)) {
+
+            if (age >= 16) {
+                System.out.println("Mr.");
+
+            } else {
+
+                System.out.println("Master");
+            }
+
+        }
+        if ("f".equals(gender)) {
+
+            if (age >= 16) {
+                System.out.println("Ms.");
+            } else {
+                System.out.println("Miss");
+            }
+        }
+    }
+}
