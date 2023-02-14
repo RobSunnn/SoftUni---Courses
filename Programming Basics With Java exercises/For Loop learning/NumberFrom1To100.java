1545ef511cba12e6699defeaf301b73e7e9edbff
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-02-14 14:11:10 +0200

    Added some more exercises from the first time I met forLoop with java

---
+++ Programming Basics With Java exercises/For Loop learning/NumberFrom1To100.java
@@ -0,0 +1,10 @@
+package ForLoopLab;
+
+public class NumberFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(num);
+        }
+    }
+}
