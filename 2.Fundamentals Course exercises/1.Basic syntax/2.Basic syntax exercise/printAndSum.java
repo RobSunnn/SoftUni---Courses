98d07fb24b524d3f23b597e2186770fbdd4a3743
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-03-01 19:33:15 +0200

    Added more exercises from Fundamentals module in SoftUni in Basic syntax exercise folder

---
+++ 2.Fundamentals Course exercises/1.Basic syntax/2.Basic syntax exercise/printAndSum.java
@@ -0,0 +1,18 @@
+package fundamentals.P02.basicSyntax.exercise;
+
+import java.util.Scanner;
+
+public class printAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        for (int i = startNumber; i <= endNumber; i++) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+        System.out.printf("%nSum: %d", sum);
+    }
+}
