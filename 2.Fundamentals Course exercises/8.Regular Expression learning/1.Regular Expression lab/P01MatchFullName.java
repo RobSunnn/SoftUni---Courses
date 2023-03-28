90b7012185df5f6ca7c9b4b60d16f1a84ba50781
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-03-28 19:31:32 +0300

    Added exercises in folder Regular Expressions learning  from Fundamentals module in SoftUni

---
+++ 2.Fundamentals Course exercises/8.Regular Expression learning/1.Regular Expression lab/P01MatchFullName.java
@@ -0,0 +1,21 @@
+package fundamentals.P16.RegularExpression.lab;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class P01MatchFullName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("\\b[A-Z][a-z]+ [A-Z][a-z]+\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            String name = matcher.group();
+            System.out.print(name + " ");
+        }
+    }
+}
