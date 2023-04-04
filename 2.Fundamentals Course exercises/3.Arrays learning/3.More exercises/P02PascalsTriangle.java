e6d4b2ed7e477daf39c0eb6433cf7f4773ab060e
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-04-04 21:34:50 +0300

    Added exercises in folder Arrays  -> More exercises from Fundamentals module in SoftUni

---
+++ 2.Fundamentals Course exercises/3.Arrays learning/3.More exercises/P02PascalsTriangle.java
@@ -0,0 +1,24 @@
+package More.Exercise.Arrays;
+
+import java.util.Scanner;
+
+public class P02PascalsTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+
+            int number = 1;
+
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
