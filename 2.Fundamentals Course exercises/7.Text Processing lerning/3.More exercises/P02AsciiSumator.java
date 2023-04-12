9cc1ff3107efc6c5600db59459f5e717c9af14c3
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-04-12 21:12:44 +0300

    Added exercises in folder Text processing more exercises from Fundamentals module in SoftUni

---
+++ 2.Fundamentals Course exercises/7.Text Processing lerning/3.More exercises/P02AsciiSumator.java
@@ -0,0 +1,28 @@
+package MoreExercise.Text.Processing;
+
+import java.util.Scanner;
+
+public class P02AsciiSumator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char first = scanner.nextLine().charAt(0);
+        char second = scanner.nextLine().charAt(0);
+        String text = scanner.nextLine();
+
+        int sum = 0;
+
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char current = text.charAt(i);
+
+            if (current > first && current < second) {
+
+                sum += current;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
