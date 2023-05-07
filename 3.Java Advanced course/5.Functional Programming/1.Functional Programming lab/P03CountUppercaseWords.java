4367d9e0aa9063076fd345fb6b50daa69acc10af
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-05-07 19:42:25 +0300

    Added exercises in folder Functional Programming lab from Java Advanced module in SoftUni

---
+++ 3.Java Advanced course/5.Functional Programming/1.Functional Programming lab/P03CountUppercaseWords.java
@@ -0,0 +1,23 @@
+package P09FunctionalProgramming.lab;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P03CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Predicate<String> startsWithUpperCase = word -> Character.isUpperCase(word.charAt(0));
+        Consumer<String> print = str -> System.out.println(str);
+
+        List<String> uppercaseWords = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(word -> startsWithUpperCase.test(word)).collect(Collectors.toList());
+
+        System.out.println(uppercaseWords.size());
+
+        uppercaseWords.forEach(elem -> print.accept(elem));
+    }
+}
