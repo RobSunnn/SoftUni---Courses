acce62d8e54c79ef5b24206feee139168736efa4
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-05-15 20:23:39 +0300

    Added exercises in folder Iterators And Comparators lab from Java Advanced module in SoftUni

---
+++ 3.Java Advanced course/Iterators And Comparators/Iterators And Comparators lab/Book/Book.java
@@ -0,0 +1,57 @@
+package P14IteratorsAndComparators.Book;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book> {
+
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    private void setAuthors(String... authors) {
+        if (authors.length == 0) {
+            this.authors = new ArrayList<String>();
+        } else {
+            this.authors = new ArrayList<>(Arrays.asList(authors));
+
+        }
+    }
+
+    @Override
+    public int compareTo(Book o) {
+      int result = this.getTitle().compareTo(o.getTitle());
+      if (result != 0) {
+          return result;
+      } else {
+          return Integer.compare(this.getYear(), o.getYear());
+      }
+    }
+}
