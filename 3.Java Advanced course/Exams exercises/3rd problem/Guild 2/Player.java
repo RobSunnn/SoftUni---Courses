34c8aea9755ec449c850790c633bdc37714fc242
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-06-16 18:57:31 +0300

    Added exercises in folder Exams exercises  from Java Advanced module in SoftUni

---
+++ 3.Java Advanced course/Exams exercises/3rd problem/Guild 2/Player.java
@@ -0,0 +1,59 @@
+package guild;
+
+public class Player {
+    private String name;
+    private String clazz;
+    private String rank;
+    private String description;
+
+    public Player(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+        this.rank = "Trial";
+        this.description = "n/a";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(String.format("Player %s: %s", this.name, this.clazz))
+                .append(System.lineSeparator());
+        stringBuilder.append(String.format("Rank: %s", this.rank))
+                .append(System.lineSeparator());
+        stringBuilder.append(String.format("Description: %s",this.description));
+
+        return stringBuilder.toString().trim();
+    }
+}
