921d64d387cb16ea4cbf8cba7d28ff13a9d4df13
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-07-01 21:44:20 +0300

    Added exercises in folder Exams exercises  from Java OOP course in SoftUni

---
+++ Java OOP course/Exams exercises/Bakery/src/bakery/entities/bakedFoods/Bread.java
@@ -0,0 +1,10 @@
+package bakery.entities.bakedFoods;
+
+public class Bread extends BaseFood{
+
+    public static final double INITIAL_BREAD_PORTION = 200.0;
+
+    public Bread(String name, double price) {
+        super(name, INITIAL_BREAD_PORTION, price);
+    }
+}
