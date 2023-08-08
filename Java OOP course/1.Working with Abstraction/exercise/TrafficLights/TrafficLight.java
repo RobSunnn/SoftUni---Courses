36ff336b71f3648b78d304754feb4f5477d8a423
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-08-08 20:01:41 +0300

    Added exercises in folder Working with abstraction exercises  from Java OOP course in SoftUni

---
+++ Java OOP course/1.Working with Abstraction/exercise/TrafficLights/TrafficLight.java
@@ -0,0 +1,23 @@
+package Abstraction.exercise.TrafficLights;
+
+public enum TrafficLight {
+    GREEN, YELLOW, RED;
+
+
+    public TrafficLight changeLight(TrafficLight trafficLight) {
+        switch (trafficLight) {
+            case GREEN:
+                trafficLight = YELLOW;
+                break;
+            case YELLOW:
+                trafficLight = RED;
+                break;
+            case RED:
+                trafficLight = GREEN;
+                break;
+        }
+        return trafficLight;
+    }
+
+
+}
