de1a9556e0870630bbd1bbe1a474b40729914b40
Rob$rt0 <RobertoSpasovrs@gmail.com>
2023-09-25 18:40:50 +0300

    Added more exercises from my Java OOP course in SoftUni

---
+++ Java OOP course/Polymorphism/lab/_03_Animals/Cat.java
@@ -0,0 +1,14 @@
+package Polymorphism.lab._03_Animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%n" +
+                "MEEOW", getName(), getFavouriteFood());
+    }
+}
---
+++ Java OOP course/Polymorphism/lab/_04_WildFarm/Meat.java
@@ -0,0 +1,8 @@
+package Polymorphism.lab._04_WildFarm;
+
+public class Meat extends Food {
+
+    protected Meat(Integer quantity) {
+        super(quantity);
+    }
+}
